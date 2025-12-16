399e2ebc6e6853b590a0b4c84012042455e508e6
Pravin T <pravint2006@gmail.com>
2025-12-16 20:29:12 +0000

    File changes

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,14 +1,15 @@
+
 import { initializeApp, getApps, getApp, type FirebaseOptions } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig: FirebaseOptions = {
+  projectId: "studio-9781884869-4d982",
+  appId: "1:325810737238:web:e3460f5bf36cdf10f2ebf7",
   apiKey: "AIzaSyAsX9NgE2I_DKnfKXDl3k75GH4L2dLgRFg",
   authDomain: "studio-9781884869-4d982.firebaseapp.com",
-  projectId: "studio-9781884869-4d982",
-  storageBucket: "studio-9781884869-4d982.appspot.com",
   messagingSenderId: "325810737238",
-  appId: "1:325810737238:web:e3460f5bf36cdf10f2ebf7",
+  storageBucket: "studio-9781884869-4d982.appspot.com"
 };
 
 // Initialize Firebase
