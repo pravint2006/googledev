163fbdeb2f3f09878666717c337fd2c7b1615d67
Pravin T <pravint2006@gmail.com>
2025-12-31 18:12:10 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-9781884869-4d982",
-  "appId": "1:325810737238:web:e3460f5bf36cdf10f2ebf7",
-  "apiKey": "AIzaSyAsX9NgE2I_DKnfKXDl3k75GH4L2dLgRFg",
-  "authDomain": "studio-9781884869-4d982.firebaseapp.com",
+  "projectId": "dev-61141163-629c6",
+  "appId": "1:613004193290:web:4c09f519cf1e6e107d39f3",
+  "apiKey": "AIzaSyAf0BpW4HGxWmnKF8Os6pl3SWcCpZ7spMA",
+  "authDomain": "dev-61141163-629c6.firebaseapp.com",
   "measurementId": "",
-  "messagingSenderId": "325810737238"
+  "messagingSenderId": "613004193290"
 };
