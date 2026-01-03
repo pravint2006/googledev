959558ef3c69185edb474121a47658c9653f3bc2
Pravin T <pravint2006@gmail.com>
2026-01-03 17:15:56 +0000

    ok what about the firebase api key where to get that

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,10 +4,11 @@
 
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: "AIzaSyAf0BpW4HGxWmnKF8Os6pl3SWcCpZ7spMA",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "dev-61141163-629c6.firebaseapp.com",
   projectId: "dev-61141163-629c6",
   storageBucket: "dev-61141163-629c6.appspot.com",
   messagingSenderId: "613004193290",
   appId: "1:613004193290:web:4c09f519cf1e6e107d39f3"
 };
+
