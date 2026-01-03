d9c7d768417eebb4cb436daa3e96a0faf79190bd
Pravin T <pravint2006@gmail.com>
2026-01-03 16:09:01 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,13 @@
 
+// This file exports the configuration object needed to initialize the Firebase SDK.
+// It is used in /src/firebase/index.ts.
+
+// Your web app's Firebase configuration
 export const firebaseConfig = {
-  "projectId": "dev-61141163-629c6",
-  "appId": "1:613004193290:web:4c09f519cf1e6e107d39f3",
-  "storageBucket": "dev-61141163-629c6.appspot.com",
-  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  "authDomain": "dev-61141163-629c6.firebaseapp.com",
-  "messagingSenderId": "613004193290"
+  apiKey: "AIzaSyAf0BpW4HGxWmnKF8Os6pl3SWcCpZ7spMA",
+  authDomain: "dev-61141163-629c6.firebaseapp.com",
+  projectId: "dev-61141163-629c6",
+  storageBucket: "dev-61141163-629c6.appspot.com",
+  messagingSenderId: "613004193290",
+  appId: "1:613004193290:web:4c09f519cf1e6e107d39f3"
 };
