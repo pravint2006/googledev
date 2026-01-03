c6afe1af347fc1881821b0b682a35682c85cbbc4
Pravin T <pravint2006@gmail.com>
2026-01-03 16:01:46 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -32,10 +32,6 @@ const nextConfig: NextConfig = {
       }
     ],
   },
-  env: {
-    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
-    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-  }
 };
 
 export default nextConfig;
